46e92119876bda82e9315c600ac44897c983c6b6
Mohzcrea8me <mohzcrea8me@gmail.com>
2024-08-28 21:04:50 +0100

    heap change

--- contracts/Heap.sol
+++ contracts/Heap.sol
@@ -1,183 +1,209 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.6.0;
+pragma solidity ^0.7.0;
 
-import "@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
+// import "hardhat/console.sol";
 
 contract Heap {
-    using SafeMath for uint256;
 
-    uint256[] public _orderbook;
+    uint256[] internal _orderbook;
 
-    // Inserts adds in a value to our heap.
-    //_value is price in the orderbook, _ref is order reference
-    function insertBuyOrders(uint256 _value) public {
-        // Add the value to the end of our array
+    // Inserts a buy order into buyorderbook and put order with highest price at first position
+    function insertBuyOrder(uint256 _value) external {
+        //insert buy order
         _orderbook.push(_value);
-
-        // Start at the end of the array
-        uint256 currentIndex = SafeMath.sub(_orderbook.length, 1);
-
-        // Bubble up the value until it reaches it's correct place (i.e. it is smaller than it's parent)
-        uint256 parentIndex = SafeMath.div(currentIndex, 2);
-        while (
-            currentIndex > 0 &&
-            _orderbook[parentIndex] <
-            _orderbook[currentIndex]
-        ) {
-            // If the parent value is lower than our current value, we swap them
-            uint256 temp = _orderbook[parentIndex];
-            _orderbook[parentIndex] = _orderbook[currentIndex];
-            _orderbook[currentIndex] = temp;
-
-            // change our current Index to go up to the parent
-            currentIndex = parentIndex;
-            parentIndex = SafeMath.div(currentIndex, 2);
-        }
+        uint256 currentIndex = _orderbook.length - 1;
+        //push order with highest price to first position
+        _bubbleUpForMax(currentIndex);
     }
 
-    function insertSellOrders(uint256 _value) public {
-        // Add the value to the end of our array
+    // Inserts a sell order into sellorderbook and put order with lowest price at first position
+    function insertSellOrder(uint256 _value) external {
+        //insert buy order
         _orderbook.push(_value);
-
-        // Start at the end of the array
-        uint256 currentIndex = SafeMath.sub(_orderbook.length, 1);
-
-        // Bubble up the value until it reaches it's correct place (i.e. it is smaller than it's parent)
-        uint256 parentIndex = SafeMath.div(currentIndex, 2);
-        while (
-            currentIndex > 0 &&
-            _orderbook[parentIndex] >
-            _orderbook[currentIndex]
-        ) {
-            // If the parent value is lower than our current value, we swap them
-            uint256 temp = _orderbook[parentIndex];
-            _orderbook[parentIndex] = _orderbook[currentIndex];
-            _orderbook[currentIndex] = temp;
-
-            // change our current Index to go up to the parent
-            currentIndex = parentIndex;
-            parentIndex = SafeMath.div(currentIndex, 2);
-        }
+        uint256 currentIndex = _orderbook.length - 1;
+        //push order with lowest price to first position
+        _bubbleUpForMin(currentIndex);
     }
 
-    // RemoveMax pops off the root element of the heap (the highest value here) and rebalances the heap
-    // This function is to be used when we need to find the max buy price for a new sell order 
-    function removeMax() public returns (uint256) {
-        // Ensure the heap exists
-        require(_orderbook.length > 0, "Orderbook is not initialized");
-
-        // take the root value of the heap
-        uint256 toReturn = _orderbook[0];
-
-        // Takes the last element of the array and put it at the root
-        _orderbook[0] = _orderbook[SafeMath.sub(_orderbook.length, 1)];
-
-        // Delete the last element from the array
-        _orderbook.pop();
-
-        // Start at the top
-        uint256 currentIndex = 0;
-
-        // Bubble down
-        bubbleDownForMax(currentIndex);
-
-        // finally, return the top of the heap
-        return toReturn;
-    }
-
-    // This function is to be used when we need to find the min sell price for a new buy order 
-    function removeMin() public returns(uint256){
+    // Remove, return buy order with the highest price and balance buyorders heap
+    //position the highest price buy order after the one removed to first position
+    function removeTop() external returns (uint256) {
         require(_orderbook.length > 0, "Orderbook is empty");
+        uint256 toReturn = _orderbook[0]; //buy order with highest price
+        uint256 lastIndex = _orderbook.length - 1;
 
-        /*uint256 toReturn = _orderbook[_orderbook.length - 1];
-
-        if (_orderbook.length == 1) {
+        if (lastIndex > 0) {
+            //replace first order with last order
+            _orderbook[0] = _orderbook[lastIndex];
+            //delete last order  //Todo: No Amount/value filled check????
             _orderbook.pop();
-            return toReturn;
-        }*/
-        uint256 toReturn = _orderbook[0];   
-
-        // Takes the last element of the array and put it at the root
-        _orderbook[0] = _orderbook[SafeMath.sub(_orderbook.length, 1)];
-
-        // Delete the last element from the array
-        _orderbook.pop();     
-
-        // Start at the top
-        uint256 currentIndex = 0;
-
-        // Bubble down
-        bubbleDownForMin(currentIndex);
-
+            //rebalance buy orders heap to push highest price to first position 
+            //startIndex is 0
+            _bubbleDownForMax(0);
+        } else {
+            //if just one order delete it and move on
+            _orderbook.pop();
+        }
+        //return order with highest price
         return toReturn;
     }
 
-    function bubbleDownForMax(uint256 currentIndex) public {
-        while (SafeMath.mul(currentIndex, 2) < SafeMath.sub(_orderbook.length, 1)) {
-            // get the current index of the children
-            uint256 j = SafeMath.mul(currentIndex, 2);
+    // Remove, return the lowest price sell order and balance sellorders heap
+    //position the lowest price sell order after the one removed to first position
+    function removeMin() external returns (uint256) {
+        require(_orderbook.length > 0, "Orderbook is empty");
+        uint256 toReturn = _orderbook[0]; //sell order with lowest price
+        uint256 lastIndex = _orderbook.length - 1;
 
-            // left child value
-            uint256 leftChild = _orderbook[j];
-            // right child value
-            uint256 rightChild = _orderbook[SafeMath.add(j, 1)];
-
-            // Compare the left and right child. if the rightChild is greater, then point j to it's index
-            if (leftChild < rightChild) {
-                j = SafeMath.add(j, 1);
-            }
-
-            // compare the current parent value with the highest child, if the parent is greater, we're done
-            if (_orderbook[currentIndex] > _orderbook[j]) {
-                break;
-            }
-
-            // else swap the value
-            uint256 temporaryOrder = _orderbook[currentIndex];
-            _orderbook[currentIndex] = _orderbook[j];
-            _orderbook[j] = temporaryOrder;
-
-            // and let's keep going down the heap
-            currentIndex = j;
+        if (lastIndex > 0) {
+            //replace first order with last order
+            _orderbook[0] = _orderbook[lastIndex];
+             //delete last order //Todo: No Amount/value filled check????
+            _orderbook.pop();
+             //rebalance sell orders heap to push lowest price to first position
+             //startIndex is 0
+            _bubbleDownForMin(0);
+        } else {
+            //if just one order delete it and move on
+            _orderbook.pop();
         }
+        return toReturn;
     }
 
-    function bubbleDownForMin(uint256 currentIndex) public {
-        while (SafeMath.mul(currentIndex, 2) < SafeMath.sub(_orderbook.length, 1)) {
-            // get the current index of the children
-            uint256 j = SafeMath.mul(currentIndex, 2);
 
-            // left child value
-            uint256 leftChild = _orderbook[j];
-            // right child value
-            uint256 rightChild = _orderbook[SafeMath.add(j, 1)];
-
-            // Compare the left and right child. if the leftChild is less, then point j to its index
-            if (leftChild > rightChild) {
-                j = SafeMath.add(j, 1);
-            }
-
-            // compare the current parent value with the lowest child, if the parent is less, we're done
-            if (_orderbook[currentIndex] < _orderbook[j]) {
-                break;
-            }
-
-            // else swap the value
-            uint256 temporaryOrder = _orderbook[currentIndex];
-            _orderbook[currentIndex] = _orderbook[j];
-            _orderbook[j] = temporaryOrder;
-
-            // and let's keep going down the heap
-            currentIndex = j;
-        }
-    }
-
-    function getOrderbook() public view returns (uint256[] memory) {
+    //return allsellorderbook
+    function getOrderbook() external view returns (uint256 [] memory) {
         return _orderbook;
     }
 
-    function getTop() public view returns (uint256) {
+
+
+    //return buy order with highest price
+    function getTop() external view returns (uint256) {
+        require(_orderbook.length > 0, "Buy Orderbook is empty");
         return _orderbook[0];
+    }
+
+
+    function _bubbleUpForMax(uint256 currentIndex) private {
+        //skip heap balancing for first buy order and orders that are rebalanced to first position;
+        while (currentIndex > 0) {
+            // get parentIndex using formula's constant
+            uint256 parentIndex = (currentIndex - 1) / 2;
+            // console.logUint(parentIndex);
+
+            //stop heap balancing if parentOrder price is greater than new/updated-new order price
+            if (_orderbook[currentIndex] <= _orderbook[parentIndex]) {
+                break;
+            }
+            //swap order positions till higher/highest price is at first position
+            _swapBuyOrder(currentIndex, parentIndex);
+            //update current index to parent for buy orders heap-rebalancing
+            currentIndex = parentIndex;
+        }
+    }
+
+    function _bubbleUpForMin(uint256 currentIndex) private {
+        //skip heap balancing for first sell order and orders that are rebalanced to first position;
+        while (currentIndex > 0) {
+             // get parentIndex using formula's constant
+            uint256 parentIndex = (currentIndex - 1) / 2;
+            // console.logUint(parentIndex);
+
+            //stop heap balancing if parentOrder price is lesser than new/updated-new order price
+            if (_orderbook[currentIndex] >= _orderbook[parentIndex]) {
+                break;
+            }
+            //swap order positions till lower/lowest price is at first position
+            _swapSellOrder(currentIndex, parentIndex);
+             //update current index to parent for sell orders heap-rebalancing
+            currentIndex = parentIndex;
+        }
+    }
+
+    //===== Bubble Down Formula =====
+        //startIndex(leftChild) = a * 2 + 1
+        //nextIndex(rightChild) = a * 2 + 2
+        //since orders are inserted using the logic from Bubble Up
+        //a consecutive run through can be done for orders starting with next 2 orders at a calculated position from current/lastchecked order
+        //comparing them to current order or last checked order for heap balancing
+    
+    function _bubbleDownForMax(uint256 currentIndex) private {
+        uint256 length = _orderbook.length;
+        //run through all orders till heap is balance and stop
+        while (true) {
+            //start from a calculated order position since position is known from how they are inserted
+            uint256 startIndex = currentIndex * 2 + 1; 
+            //go to next order position after calculated position
+            uint256 nextIndex = currentIndex * 2 + 2; //go to the next one 
+            
+            //highest order is assumed to be current order or last checked order(for rebalancing)
+            uint256 higherOrderPriceIndex = currentIndex;
+            
+            //compare start and next order prices to largest/current order or last checked order(for rebalancing)
+            if (startIndex < length && _orderbook[startIndex] > _orderbook[higherOrderPriceIndex]) {
+                higherOrderPriceIndex = startIndex;
+            }
+
+            if (nextIndex < length && _orderbook[nextIndex] > _orderbook[higherOrderPriceIndex]) {
+                higherOrderPriceIndex = nextIndex;
+            }
+
+            //Stop if both start and next order have no price higher than current;
+            if (higherOrderPriceIndex == currentIndex) break;
+            
+            //if any of start or next order has higher price 
+            //swap order positions till higher/highest price is at first position
+            _swapBuyOrder(currentIndex, higherOrderPriceIndex);
+
+            //update current order to last checked order for heap-rebalancing
+            currentIndex = higherOrderPriceIndex;
+        }
+    }
+
+    function _bubbleDownForMin(uint256 currentIndex) private {
+        uint256 length = _orderbook.length;
+        //run through all orders till heap is balance and stop
+        while (true) {
+            //start from a calculated order position since position is known from how they are inserted
+            uint256 startIndex = currentIndex * 2 + 1;
+            //go to next order position after calculated position
+            uint256 nextIndex = currentIndex * 2 + 2;
+            //lowest order is assumed to be current order or last checked order(for rebalancing)
+            uint256 lowestOrderPriceIndex = currentIndex;
+
+             //compare start and next order prices to lowest/current order or last checked order(for rebalancing)
+            if (startIndex < length && _orderbook[startIndex] < _orderbook[lowestOrderPriceIndex]) {
+                lowestOrderPriceIndex = startIndex;
+            }
+
+            if (nextIndex < length && _orderbook[nextIndex] < _orderbook[lowestOrderPriceIndex]) {
+                lowestOrderPriceIndex = nextIndex;
+            }
+            //Stop if both start and next order have no price lower than current;
+            if (lowestOrderPriceIndex == currentIndex) break;
+
+            //if any of start or next order has lower price 
+            //swap order positions till lower/lowest price is at first position
+            _swapSellOrder(currentIndex, lowestOrderPriceIndex);
+
+            //update current order to last checked order for heap-rebalancing
+            currentIndex = lowestOrderPriceIndex;
+        }
+    }
+
+    //swap buy orders positions and their ref => index mapping
+    function _swapBuyOrder(uint256 i, uint256 j) private {
+        uint256 temp = _orderbook[i];
+        _orderbook[i] = _orderbook[j];
+        _orderbook[j] = temp;
+    }
+
+    //swap sell orders positions and their ref => index mapping
+    function _swapSellOrder(uint256 i, uint256 j) private {
+        uint256 temp = _orderbook[i];
+        _orderbook[i] = _orderbook[j];
+        _orderbook[j] = temp;
     }
 
 }
--- truffle-config.js
+++ truffle-config.js
@@ -73,6 +73,11 @@ module.exports = {
     // }
   },
 
+  solidityLog: {
+    displayPrefix: "",
+    preventConsoleMigration: true
+  },
+
   // Set default mocha options here, use special reporters etc.
   mocha: {
     reporter: 'eth-gas-reporter',
@@ -93,7 +98,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.6.0",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.7.1",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
